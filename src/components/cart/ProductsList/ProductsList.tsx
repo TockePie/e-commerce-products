c123f3dfb0d93bee07c58493ff39a626522446e9
TockePie <144056146+TockePie@users.noreply.github.com>
2024-09-25 12:16:30 +0300

    Fixed theme and cart

--- src/components/cart/ProductsList/ProductsList.tsx
+++ src/components/cart/ProductsList/ProductsList.tsx
@@ -6,6 +6,7 @@ import calculateDiscountedPrice from "@/utils/calculateDiscountedPrice";
 import { ProductType } from "@/types/product";
 
 import styles from "./ProductsList.styles";
+import ThemeWrapper from "@/components/ThemeWrapper";
 
 const ProductsList = ({
   parsedCartItems,
@@ -15,62 +16,66 @@ const ProductsList = ({
   const router = useRouter();
 
   return (
-    <List>
-      {parsedCartItems.map((product: ProductType) => {
-        const discount = calculateDiscountedPrice(
-          product.price,
-          product.discountPercentage
-        );
+    <ThemeWrapper>
+      <List>
+        {parsedCartItems.map((product: ProductType) => {
+          const discount = calculateDiscountedPrice(
+            product.price,
+            product.discountPercentage
+          );
 
-        return (
-          <ListItem key={product.id}>
-            <ListItemButton
-              sx={styles.itemButton}
-              onClick={() => {
-                router.push(`/${product.id}`);
-              }}
-            >
-              <Box sx={styles.product}>
-                {product.images.length > 0 && (
-                  <Image
-                    src={product.images[0]}
-                    alt={product.title}
-                    width={50}
-                    height={50}
-                    style={{ objectFit: "contain" }}
-                  />
-                )}
-                <Typography>{product.title}</Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                >{`x${product.minimumOrderQuantity}`}</Typography>
-              </Box>
-              <Box sx={styles.priceBox}>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{
-                    textDecoration: product.discountPercentage
-                      ? "line-through"
-                      : "none",
-                  }}
-                >
-                  {`$${(product.price * product.minimumOrderQuantity).toFixed(
-                    2
-                  )}`}
-                </Typography>
-                {product.discountPercentage && (
-                  <Typography variant="body1" color="red">
-                    {`$${(discount * product.minimumOrderQuantity).toFixed(2)}`}
+          return (
+            <ListItem key={product.id}>
+              <ListItemButton
+                sx={styles.itemButton}
+                onClick={() => {
+                  router.push(`/${product.id}`);
+                }}
+              >
+                <Box sx={styles.product}>
+                  {product.images.length > 0 && (
+                    <Image
+                      src={product.images[0]}
+                      alt={product.title}
+                      width={50}
+                      height={50}
+                      style={{ objectFit: "contain" }}
+                    />
+                  )}
+                  <Typography>{product.title}</Typography>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  >{`x${product.minimumOrderQuantity}`}</Typography>
+                </Box>
+                <Box sx={styles.priceBox}>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{
+                      textDecoration: product.discountPercentage
+                        ? "line-through"
+                        : "none",
+                    }}
+                  >
+                    {`$${(product.price * product.minimumOrderQuantity).toFixed(
+                      2
+                    )}`}
                   </Typography>
-                )}
-              </Box>
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
+                  {product.discountPercentage && (
+                    <Typography variant="body1" color="red">
+                      {`$${(discount * product.minimumOrderQuantity).toFixed(
+                        2
+                      )}`}
+                    </Typography>
+                  )}
+                </Box>
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </ThemeWrapper>
   );
 };
 
